52ec22e6f6ce8a490e3f081d8ae4564d9f59dadb
Daniel Puehringer <danielpuehringer11@gmail.com>
2024-05-23 16:34:32 +0200

    wip mess

--- visualizations/StarPlot.js
+++ visualizations/StarPlot.js
@@ -1,74 +1,74 @@
 class StarPlot {
 	constructor(svg_element_id) {
-		console.log("hey")
-
-
-		let data = [];
-		let features = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-		//generate the data
-		for (var i = 0; i < 3; i++){
-			var point = {}
-			//each feature will be a random number from 1-9
-			features.forEach(f => point[f] = 1 + Math.random() * 8);
-			data.push(point);
-		}
-		console.log(data);
-		let width = 600;
-		let height = 600;
-		let svg = d3.select("body").append("svg")
-			.attr("width", width)
-			.attr("height", height);
-
-		let radialScale = d3.scaleLinear()
-			.domain([0, 10])
+		this.data = [];
+		this.features = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		this.months_indexed = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5:"May", 6:"Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"};
+		this.avgs_processed = {};
+		this.width = 600;
+		this.height = 600;
+		this.radialScale = d3.scaleLinear()
+			.domain([0, 20000000])
 			.range([0, 250]);
-		let ticks = [2, 4, 6, 8, 10];
+		this.svg = this.star_graph_set_up();
+
+		d3.json("./Data/europe_passengers/avg_by_year.json")
+			.then(load_data => this.process_avgs_by_country(load_data));
+	}
+
+	angleToCoordinate(angle, value){
+		let x = Math.cos(angle) * this.radialScale(value);
+		let y = Math.sin(angle) * this.radialScale(value);
+		return {"x": this.width / 2 + x, "y": this.height / 2 - y};
+	}
+
+	star_graph_set_up() {
+		// set up general radial circle
+		let svg = d3.select("body").append("svg")
+			.attr("width", this.width)
+			.attr("height", this.height);
+
+		let ticks = [4000000, 8000000, 12000000, 16000000, 20000000];
 		svg.selectAll("circle")
 			.data(ticks)
 			.join(
 				enter => enter.append("circle")
-					.attr("cx", width / 2)
-					.attr("cy", height / 2)
+					.attr("cx", this.width / 2)
+					.attr("cy", this.height / 2)
 					.attr("fill", "none")
 					.attr("stroke", "gray")
-					.attr("r", d => radialScale(d))
+					.attr("r", d => this.radialScale(d))
 			);
 		svg.selectAll(".ticklabel")
-		.data(ticks)
-		.join(
-			enter => enter.append("text")
-				.attr("class", "ticklabel")
-				.attr("x", width / 2 + 5)
-				.attr("y", d => height / 2 - radialScale(d))
-				.text(d => d.toString())
-		);
-		function angleToCoordinate(angle, value){
-			let x = Math.cos(angle) * radialScale(value);
-			let y = Math.sin(angle) * radialScale(value);
-			return {"x": width / 2 + x, "y": height / 2 - y};
-		}
-		let featureData = features.map((f, i) => {
-			let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
+			.data(ticks)
+			.join(
+				enter => enter.append("text")
+					.attr("class", "ticklabel")
+					.attr("x", this.width / 2 + 5)
+					.attr("y", d => this.height / 2 - this.radialScale(d))
+					.text(d => d.toString())
+			);
+		let featureData = this.features.map((f, i) => {
+			let angle = (Math.PI / 2) + (2 * Math.PI * i / this.features.length);
 			return {
 				"name": f,
 				"angle": angle,
-				"line_coord": angleToCoordinate(angle, 10),
-				"label_coord": angleToCoordinate(angle, 10.5)
+				"line_coord": this.angleToCoordinate(angle, 20000000),
+				"label_coord": this.angleToCoordinate(angle, 21500000)
 			};
 		});
-		
+
 		// draw axis line
 		svg.selectAll("line")
 			.data(featureData)
 			.join(
 				enter => enter.append("line")
-					.attr("x1", width / 2)
-					.attr("y1", height / 2)
+					.attr("x1", this.width / 2)
+					.attr("y1", this.height / 2)
 					.attr("x2", d => d.line_coord.x)
 					.attr("y2", d => d.line_coord.y)
 					.attr("stroke","black")
 			);
-		
+
 		// draw axis label
 		svg.selectAll(".axislabel")
 			.data(featureData)
@@ -78,43 +78,96 @@ class StarPlot {
 					.attr("y", d => d.label_coord.y)
 					.text(d => d.name)
 			);
+		return svg;
+	}
+
+	process_avgs_by_country(d) {
+		// map country code and each month of the year to its corresponding avg # of passengers
+		// structure -> map<country_code, map<month, avg_passengers>>
+		// example -> map<"ESP", map<"Jan", 123456789>>
+		d.forEach(measure => {
+			const { country, month, passengers } = measure;
+
+			if (!this.avgs_processed[country]) {
+				this.avgs_processed[country] = {};
+			}
+			this.avgs_processed[country][this.months_indexed[parseInt(month)]] = parseInt(passengers);
+		});
+
+		// Update display for the default country (e.g., GBR)
+		this.update_country_displayed("GBR");
+	}
+
+	update_country_displayed(ctry_code) {
+		console.log("\t inside update_country_displayed with parameter:"+ctry_code)
+		let colors = ["darkorange", "gray", "navy"];
 		let line = d3.line()
 			.x(d => d.x)
 			.y(d => d.y);
-		let colors = ["darkorange", "gray", "navy"];
-		function getPathCoordinates(data_point){
+		const getPathCoordinates = (data_point) => {
 			let coordinates = [];
-			for (var i = 0; i < features.length; i++){
-				let ft_name = features[i];
-				let angle = (Math.PI / 2) + (2 * Math.PI * i / features.length);
-				coordinates.push(angleToCoordinate(angle, data_point[ft_name]));
+			for (let i = 0; i < this.features.length; i++){
+				let ft_name = this.features[i];
+				let angle = (Math.PI / 2) + (2 * Math.PI * i / this.features.length);
+				coordinates.push(this.angleToCoordinate(angle, data_point[ft_name]));
 			}
 			return coordinates;
-		}
-		svg.selectAll("path")
-		.data(data)
-		.join(
-			enter => enter.append("path")
-				.datum(d => getPathCoordinates(d))
-				.attr("d", line)
-				.attr("stroke-width", 3)
-				.attr("stroke", (_, i) => colors[i])
-				.attr("fill", (_, i) => colors[i])
-				.attr("stroke-opacity", 1)
-				.attr("opacity", 0.5)
-		);
-    }
+		};
+		this.data.push(this.avgs_processed[ctry_code]);
+		this.svg.selectAll("path")
+			.data(this.data)
+			.join(
+				enter => enter.append("path")
+					.datum(d => getPathCoordinates(d))
+					.attr("d", line)
+					.attr("stroke-width", 3)
+					.attr("stroke", (_, i) => colors[i])
+					.attr("fill", (_, i) => colors[i])
+					.attr("stroke-opacity", 1)
+					.attr("opacity", 0.5)
+			);
+	}
+	foo(string){
+		console.log(string)
+	}
 }
 
 function whenDocumentLoaded(action) {
 	if (document.readyState === "loading") {
 		document.addEventListener("DOMContentLoaded", action);
 	} else {
-        console.log("test")
+		console.log("test")
 		action();
 	}
 }
 
 whenDocumentLoaded(() => {
 	plot_object = new StarPlot('star-plot');
+	document.addEventListener('DOMContentLoaded', () => {
+		let currentCountry = undefined
+		const countries = document.querySelectorAll('.js-country');
+
+		countries.forEach(country => {
+			country.addEventListener('click', () => {
+				// Log the clicked country's id
+				//console.log(country.id);
+				//plot_object.foo("ESP")
+				plot_object.foo("ESP")//@chatGPT: prints Uncaught TypeError: plot_object.foo is not a function
+				let countryCode = country.id
+				console.log("countrycode:"+countryCode)
+				plot_object.update_country_displayed(countryCode)
+
+				// Remove the 'selected-country' class from the previously selected country
+				if (currentCountry) {
+					document.getElementById(currentCountry).classList.remove('selected-country');
+				}
+
+				// Update the currentCountry variable
+				currentCountry = country.id;
+
+				// Add the 'selected-country' class to the clicked country
+				country.classList.add('selected-country');
+			});
+		});
+	});
 });
